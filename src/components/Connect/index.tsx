44cf574396e0f76c9fef627e11e295a24b26f223
wangsen <wangsen1@yonghui.cn>
2024-05-03 18:11:48 +0800

    chore(05-03): react redux connect 高阶组件实现原理认识

---
+++ src/components/Connect/index.tsx
@@ -0,0 +1,26 @@
+const state = {
+  getState: () => {},
+  dispatch: {},
+};
+
+function connect(mapStateToProps: Function, mapDispatchToProps: Function) {
+  return (Component: any) => {
+    const stateProps = mapStateToProps(state.getState()); // 拿到状态
+    const stateDispatch = mapDispatchToProps(state.dispatch); // 拿到当前的dispatch
+    return <Component {...stateProps} {...stateDispatch} />;
+  };
+}
+
+export default connect;
+
+// const mapStateToProps = (state: any) => {
+//   return {
+//     list: state.hot.list || [],
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch: Function) => {
+//   return {
+//     getList: dispatch({ type: '', payload: {} }),
+//   };
+// };
