2130849c39b8bcaf6aad8a833c868ae57597d697
wangsen <wangsen1@yonghui.cn>
2024-06-24 22:07:18 +0800

    chore(06-24): react 好久没写了

---
+++ src/hooks/useCountDown/index.tsx
@@ -0,0 +1,41 @@
+import { useState, useRef, useEffect } from 'react'
+// 一、 useCountDown;
+
+// 描述
+// 1s 倒计时
+// return [count, start];
+// count 开始后1s更新 10, 9, 8 , 7
+// 点击开始计时 1s 更新 count。到0停止计时，返回初始值。0点击后又开始计时。
+
+function useCountDown(defaultVal: number) {
+  const [count, setCount] = useState<number>(defaultVal)
+  const refTimer = useRef<any>()
+
+  useEffect(() => {
+    return () => {
+      stop()
+    }
+  }, [])
+
+  function stop() {
+    clearInterval(refTimer.current)
+  }
+
+  function handleStart() {
+    refTimer.current = setInterval(() => {
+      setCount((val: number) => {
+        const newVal = val - 1
+        if (newVal === 0) {
+          stop()
+          return defaultVal
+        }
+        return newVal
+      })
+    }, 1000)
+  }
+
+  return [count, handleStart] as const
+}
+
+export { useCountDown }
+// const [count, start] = useCountDown(10)
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,24 @@
 import { useEffect } from 'react'
-import { getHome } from '../../servers'
+// import { getHome } from '../../servers'
+import { useCountDown } from '../../hooks/useCountDown'
 
 function Index() {
+  const [count, start] = useCountDown(10)
   useEffect(() => {
     console.log('执行')
-    getHome().then(res => {
-      console.log(res, ' res')
-    })
+    // getHome().then(res => {
+    //   console.log(res, ' res')
+    // })
   }, [])
-  return <div>this is index</div>
+  const handleStart = () => {
+    start()
+  }
+  return (
+    <div>
+      <h3>this is index {count}</h3>
+      <button onClick={handleStart}>click</button>
+    </div>
+  )
 }
 
 export default Index
